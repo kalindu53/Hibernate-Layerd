7914dd3825296a6fb562823e1ff0ff6b43e5d3c7
Kalindu53 <ktdkakalanka@gmai.com>
2024-09-20 09:45:14 +0530

    Add Customer form

---
+++ src/main/java/org/example/controller/CustomerFormController.java
@@ -0,0 +1,68 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.event.ActionEvent;
+
+public class CustomerFormController {
+
+    public AnchorPane rootnode;
+    @FXML
+    private TextField InputName;
+
+    @FXML
+    private TextField CustomerID;
+
+    @FXML
+    private TextField InputAdddress;
+
+    @FXML
+    private TextField InputContact;
+
+    @FXML
+    private TableColumn<?, ?> tblID;
+
+    @FXML
+    private TableColumn<?, ?> tblName;
+
+    @FXML
+    private TableColumn<?, ?> tblAddress;
+
+    @FXML
+    private TableColumn<?, ?> tblContact;
+
+    @FXML
+    void btnSDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    public void btnSaveOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void btnSDeleteOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+}
